43f0fa80c725fa86830e87f97eed6efec4b9ea4d
yaffenator <noamyaffe19@gmail.com>
2025-04-05 15:19:47 -0700

    schpiffup

--- frontend/app/generate/page.tsx
+++ frontend/app/generate/page.tsx
@@ -3,5 +3,5 @@ import Generate from "../components/sections/Generate";
 export default function Home() {
   <section className="flex w-screen h-screen bg-gray-900">
     <Generate />
-  </section>;
+  </section>
 }
