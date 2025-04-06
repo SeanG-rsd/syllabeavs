29f28e760dc1415e3b15f88802dbd9455437955f
yaffenator <noamyaffe19@gmail.com>
2025-04-05 23:51:10 -0700

    Added no classes state

--- frontend/app/components/sections/Generate.tsx
+++ frontend/app/components/sections/Generate.tsx
@@ -23,8 +23,7 @@ export default function Generate() {
       setInput("");
       setShowModal(false);
       setShake(false);
-    }
-    else {
+    } else {
       setShake(true);
 
       setTimeout(() => {
@@ -77,25 +76,23 @@ export default function Generate() {
           <div className="flex justify-center">
             <div className="w-[90%] space-y-5 flex flex-col">
               <h1 className="text-start text-white">Your classes</h1>
-              {Object.keys(syllabi).map((name) => (
-                name == currentClass
-                  ? (
-                    <button
-                      onClick={() => updateCurrentClass(name)}
-                      className="text-white flex w-full items-center rounded-lg py-2 px-3 hover:cursor-pointer bg-[#292929]"
-                    >
-                      {name}
-                    </button>
-                  )
-                  : (
-                    <button
-                      onClick={() => updateCurrentClass(name)}
-                      className="text-white flex w-full items-center rounded-lg py-2 px-3 hover:cursor-pointer hover:bg-[#292929]"
-                    >
-                      {name}
-                    </button>
-                  )
-              ))}
+              {Object.keys(syllabi).map((name) =>
+                name == currentClass ? (
+                  <button
+                    onClick={() => updateCurrentClass(name)}
+                    className="text-white flex w-full items-center rounded-lg py-2 px-3 hover:cursor-pointer bg-[#292929]"
+                  >
+                    {name}
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => updateCurrentClass(name)}
+                    className="text-white flex w-full items-center rounded-lg py-2 px-3 hover:cursor-pointer hover:bg-[#292929]"
+                  >
+                    {name}
+                  </button>
+                )
+              )}
               {Object.keys(syllabi).length == 0 ? (
                 <div />
               ) : (
@@ -139,7 +136,11 @@ export default function Generate() {
                         </label>
                       </div>
                       <div className="flex px-3 mt-4 justify-between">
-                        <div className={`text-sm ${shake ? "text-red-700" : "text-slate-500"} ${shake ? "shake-text" : ""}`}>
+                        <div
+                          className={`text-sm ${
+                            shake ? "text-red-700" : "text-slate-500"
+                          } ${shake ? "shake-text" : ""}`}
+                        >
                           <p>Input fields marked with *</p>
                           <p>are required</p>
                         </div>
@@ -178,7 +179,7 @@ export default function Generate() {
         </div>
         <div className="h-screen col-span-4 py-10">
           <div className="mr-8">
-            <Navigation  />
+            <Navigation />
           </div>
           <div className="flex h-full justify-center items-center">
             {Object.keys(syllabi).length != 0 ? (
@@ -253,7 +254,41 @@ export default function Generate() {
                 </div>
               )
             ) : (
-              <div></div>
+              <button onClick={() => setShowModal(true)} className="group hover:cursor-pointer rounded-lg  hover:border hover:border-dashed hover:border-slate-100 transition-all duration-200">
+                <div className="flex h-fit items-center justify-center">
+                  <div className="h-full p-24 space-y-10 rounded-xl">
+                    <div className="flex justify-center items-center">
+                      <svg
+                        className="size-20 group-hover:rotate-180 transition duration-200 text-slate-100 dark:text-slate-100"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                      >
+                        <g clip-path="url(#a)">
+                          <path
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="m10.4149 10.7623.0005.0109m3.0868 3.0764.0005.0108M8.91554 15.349l.00046.0108m-.8276-8.44549L4.39857 19.9133l12.95163-3.7371m-.8271-8.43475c2.0971 2.09707 3.269 4.77055 3.5172 7.51635.067.7413-.4619 1.3752-1.1869 1.5293-1.0146.2158-1.9613-.5811-2.0926-1.615-.2412-1.9-.9437-3.5721-2.52-5.1484-1.5779-1.57793-3.3173-2.3457-5.25302-2.61955-1.02139-.1445-1.79555-1.1099-1.5387-2.10314.17236-.66653.76818-1.14208 1.45754-1.08543 2.78088.22851 5.49388 1.40332 7.61648 3.52587Z"
+                          />
+                        </g>
+                        <defs>
+                          <clipPath id="a">
+                            <path fill="#fff" d="M0 0h24v24H0z" />
+                          </clipPath>
+                        </defs>
+                      </svg>
+                    </div>
+                    <p className="text-center text-slate-100 text-xl font-semibold">
+                      Looks like you haven't added any classes yet... lame...
+                    </p>
+                  </div>
+                </div>
+              </button>
             )}
           </div>
         </div>
